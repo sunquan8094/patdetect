1b000e4af86fed8f6582c2e4d12cda68f0609117
dchan3 <dchan3@hawaii.edu>
2017-03-20 19:33:02 -1000

    Added optional function field to patterns

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const ls = require("ls");
+const Discogs = require("disconnect").Client;
 var str = process.argv[2];
 
 var contents = {};
@@ -8,11 +9,12 @@ ls("./*", {type: 'file'}, /regexps_.*json/, function(file) {
   contents.regexps.push(JSON.parse(fs.readFileSync(file.file, 'utf8')));
 });
 
+var config = JSON.parse(fs.readFileSync('settings.json', 'utf8'));
+
 var retval = [];
+var funk_q = [];
 
 var r = 0;
-
-console.log(contents.regexps);
 
 for (r = 0; r < contents.regexps.length; r++) {
   console.log(r);
@@ -29,6 +31,7 @@ if (r == contents.regexps.length) {
 for (var p = 0; p < contents.regexps[r].patterns.length; p++) {
   if (str.match(new RegExp("^" + contents.regexps[r].patterns[p].pattern + "$"))) {
     retval.push(contents.regexps[r].patterns[p].type["en-US"]);
+    funk_q.push(contents.regexps[r].patterns[p].func);
   }
 }
 
@@ -39,5 +42,13 @@ else {
   console.log("Chances are this string could be one of the following:");
   for (var g = 0; g < retval.length; g++) {
     console.log("- " + retval[g]);
+    if (funk_q[g] !== "0")
+    {
+      eval(funk_q[g]);
+      func(str, function(err, data)
+      {
+        if (!err) console.log("* " + data.results[0].title);
+      });
+    }
   }
 }
